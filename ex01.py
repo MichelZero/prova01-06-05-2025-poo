d58addff6cd80acac6e9438c3a068f72f52522e3
Michel <106674127+MichelZero@users.noreply.github.com>
2025-05-06 10:23:54 -0300

    up

--- ex01.py
+++ ex01.py
@@ -1,3 +1,15 @@
 # autor: Michel
 # data: 06/05/2025
 
+# Loop infinito que só quebra quando o valor for válido
+while True:
+  # Solicita a nota ao usuário
+        nota = float(input("Digite uma nota entre 0 e 10: "))
+        # Verifica se a nota está dentro do intervalo válido
+        if 0 <= nota <= 10:
+            print("Nota válida:", nota)
+            break  # Sai do loop se a nota for válida
+        else:
+            print("Nota inválida. Digite um valor entre 0 e 10.")
+  # Loop infinito que só quebra quando o valor for válido
+        print("Entrada inválida. Digite um número.")
