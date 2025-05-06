ff604b54b1d0b8aac458abb23458ccaad8cacf14
Michel <106674127+MichelZero@users.noreply.github.com>
2025-05-06 10:38:07 -0300

    up

--- ex02.py
+++ ex02.py
@@ -1,3 +1,29 @@
 # autor: Michel
 # data: 06/05/2025
 
+# Inicializa as variáveis
+soma_impares = 0
+qtd_impares = 0
+
+# Loop infinito até que o usuário digite 0
+while True:
+    # Solicita um valor inteiro ao usuário
+    valor = int(input("Digite um valor inteiro (0 para parar): "))
+    
+    # Sai do loop se o valor for 0
+    if valor == 0:
+        break
+    
+    # Verifica se o valor é ímpar
+    if valor % 2 != 0:
+        soma_impares += valor
+        qtd_impares += 1
+        
+    print("Entrada inválida. Digite um número inteiro.")
+
+# Calcula e imprime a média dos valores ímpares, se houver
+if qtd_impares > 0:
+    media = soma_impares / qtd_impares
+    print("Média dos valores ímpares:", media)
+else:
+    print("Nenhum valor ímpar foi informado.")
--- ex03.py
+++ ex03.py
@@ -1,3 +1,29 @@
 # autor: Michel
 # data: 06/05/2025
 
+# Inicializa as variáveis com valores que garantem a atualização na primeira iteração
+maior = float('-inf') # Menor valor possível
+menor = float('inf')  # Maior valor possível
+
+
+# Loop infinito até que o usuário digite 0
+while True:
+    valor = int(input("Digite um valor inteiro (0 para parar): "))
+    if valor == 0:
+        break
+
+    # Atualiza o maior e o menor valor
+    if valor > maior:
+        maior = valor
+    if valor < menor:
+        menor = valor
+    print("Entrada inválida. Digite um número inteiro.")
+
+
+#Saida
+# Imprime o maior e o menor valor, se algum foi informado
+if maior != float('-inf') and menor != float('inf'):
+    print("Maior valor:", maior)
+    print("Menor valor:", menor)
+else:
+    print("Nenhum valor foi informado.")
